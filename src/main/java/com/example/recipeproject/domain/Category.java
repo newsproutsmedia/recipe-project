949647bf455fbdd23e4e4cffb04e89a1af6cb3a7
Nate Walters <newsproutsmedia@gmail.com>
2018-09-22 17:25:46 -0700

    Created Category, Recipe, and UOM repositories

--- src/main/java/com/example/recipeproject/domain/Category.java
+++ src/main/java/com/example/recipeproject/domain/Category.java
@@ -11,6 +11,7 @@ public class Category {
     private Long id;
     private String description;
 
+    // mappedBy is the name of the object in the related entity that controls the relationship. In this case, the "categories" object in the Recipe entity.
     @ManyToMany(mappedBy = "categories")
     private Set<Recipe> recipes;
 
---
+++ src/main/java/com/example/recipeproject/repositories/RecipeRepository.java
@@ -0,0 +1,11 @@
+package com.example.recipeproject.repositories;
+
+import com.example.recipeproject.domain.Recipe;
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface RecipeRepository extends CrudRepository<Recipe, Long> {
+
+
+
+}
